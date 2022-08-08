00fb18ea10e7e84d1cab8a035a6f5c2be97f0a59
verah <verahnyagaka101@gmail.com>
2022-08-08 16:27:09 +0300

    completed

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,10 +1,13 @@
-import React,{useState, useEffect} from 'react'
+import React,{useState} from 'react'
 import video from '../data/video'
 
 export default function VideoContainer() {
      
     const upVote= video.upvotes
+    const downVotes = video.downvotes
+    const [counts, setCounts] = useState(downVotes)
     const [count, setCount] = useState(upVote)
+    const [view, setView] = useState(true)
     function handleInc(){
        
         return(
@@ -14,7 +17,25 @@ export default function VideoContainer() {
         )
         
     }
+    function handleDislikes(){
+       
+      return(
+          setCounts(counts+1)
+      )
+      
+  }
+
+  const commentList = video.comments.map((com) =>(
+    <div>
+
+      <div key={com.id}>
+        <h3>user: {com.user}</h3>
+        <p>{com.comment}</p>
+      </div>
     
+    </div>
+  ))
+  
   return (
     <div>
     <iframe
@@ -29,13 +50,24 @@ export default function VideoContainer() {
         <h2>{video.title}</h2>
         <p>{video.views} views | created at {video.createdAt}</p>
         <button onClick={handleInc}>{count}</button>
-        <button>{video.downvotes}</button>
+        <button onClick={handleDislikes}>{counts}</button>
        
         <div>
-        <br/>
-        <button>hide comments</button>
+          <br/>
+          
+          <div>
+            <button onClick={()=>setView(!view)}>Comments</button>
+
+            <hr/>
+            {
+              view? 
+              <div key={commentList.id}>{commentList}</div>:null
+            }
+          </div>  
+      
+          <button>hide comments</button>
         </div>
-        <hr/>
+      
         
       </div>
     </div>
